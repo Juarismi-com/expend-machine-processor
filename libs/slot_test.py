ba9e7f1a0c60385995424bbf58d0c75f81671c3a
Ivan Fretes <vanfretes@gmail.com>
2025-05-21 00:48:18 -0300

    test: reles

--- libs/slot_test.py
+++ libs/slot_test.py
@@ -36,31 +36,28 @@ def activar_reles_secuencialmente(tiempo_encendido=1):
    """Activa cada relé uno por uno durante X segundos, luego lo apaga."""
    #GPIO.setup(pin, GPIO.OUT)
 
-   
-   
 
+   for nombre, pin in relays.items():
+      for nombre2, pin2 in relays.items():
+         try:
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setwarnings(True)
 
-      for nombre, pin in relays.items():
-         for nombre2, pin2 in relays.items():
-            try:
-               GPIO.setmode(GPIO.BCM)
-               GPIO.setwarnings(False)
+            GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.setup(pin2, GPIO.OUT, initial=GPIO.LOW)
+            GPIO.output(pin, GPIO.LOW)
+            GPIO.output(pin2, GPIO.LOW)
+            #print(f"{nombre} (GPIO {pin}) inicializado en LOW (relé prendido)")
+            time.sleep(1)
 
-               GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
-               GPIO.setup(pin2, GPIO.OUT, initial=GPIO.LOW)
-               GPIO.output(pin, GPIO.LOW)
-               GPIO.output(pin2, GPIO.LOW)
-               #print(f"{nombre} (GPIO {pin}) inicializado en LOW (relé prendido)")
-               time.sleep(1)
-
-            #print(f"{nombre} (GPIO {pin}) inicializado en high (relé prendido)")
-            #GPIO.cleanup()
-         except RuntimeError as e:
-            print("\nInterrumpido por el usuario.")
-            print(e)
-         finally:
-            print("finaly")
-            GPIO.cleanup()
+         #print(f"{nombre} (GPIO {pin}) inicializado en high (relé prendido)")
+         #GPIO.cleanup()
+      except RuntimeError as e:
+         print("\nInterrumpido por el usuario.")
+         print(e)
+      finally:
+         print("finaly")
+         GPIO.cleanup()
       
       
       
