beec560d0c9b3124250de65f8cf14e4192cdca90
Ivan Fretes <vanfretes@gmail.com>
2025-07-04 20:43:41 -0300

    chore: reload git ignore

--- env.py
+++
@@ -1,9 +0,0 @@
-MACHINE_ID="19bebe9a-faf1-45e9-9efc-094de55b0d6d"
-#API_URL="http://192.168.100.13:8001"
-API_URL="https://squid-app-bbs42.ondigitalocean.app"
-BANCARD_API_URL="http://192.168.100.16:3000"
-BANCARD_API_KEY="API_KEY"
-APP_PLATFORM="raspberry" #raspberry
-APP_DEBUG=False
-
-http://autvending3.local:5001/
