b1488147a46785a8c8dffd8226bbd459b85be0fe
Ivan Fretes <vanfretes@gmail.com>
2025-07-10 14:01:37 -0300

    test

--- app.py
+++ app.py
@@ -47,4 +47,4 @@ def hello_world():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8443, ssl_context=('cert.pem', 'key.pem'))
+    app.run(host='0.0.0.0', port=5001, debug=APP_DEBUG)
--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 . .venv/bin/activate
 
-export FLASK_RUN_CERT=cert.pem
-export FLASK_RUN_KEY=key.pem
-flask run --host 0.0.0.0 --port 8443 --debug
+flask run --host 0.0.0.0 --port 5001 --debug
