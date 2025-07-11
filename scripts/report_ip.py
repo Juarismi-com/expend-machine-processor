861fa8f6e60f86119c7a4c73be76e15b9b368376
Ivan Fretes <vanfretes@gmail.com>
2025-07-11 15:36:10 -0300

    feat: add script cron

--- scripts/report_ip.py
+++ scripts/report_ip.py
@@ -2,7 +2,7 @@
 import requests
 import socket
 import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from env import API_URL, MACHINE_ID
 
