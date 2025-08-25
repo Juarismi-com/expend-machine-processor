f9b3f20dc5f42baaaa1ecfc703305680fef13d25
Ivan Fretes <vanfretes@gmail.com>
2025-08-24 21:18:39 -0300

    test

--- services/dispenser_service.py
+++ services/dispenser_service.py
@@ -7,9 +7,9 @@ def select_option(option):
     print('prueba')
     if (APP_PLATFORM == "raspberry"):
         if (option == 1):
-            activate_rele(17, 4)
+            activate_rele(17, 7)
             
         if (option == 2):
-            activate_rele(26, 4)
+            activate_rele(26, 5)
 
     return option
